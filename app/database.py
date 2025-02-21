95a1097255621812d4606a2a195456679b50e2d2
Hanbo <litterpigger@gmail.com>
2025-02-21 13:01:01 +0800

    dev_file add

--- app/database.py
+++ app/database.py
@@ -3,12 +3,12 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 import redis.asyncio as redis
 from typing import AsyncGenerator
 
-# SQLALCHEMY_DATABASE_URL = "mysql+aiomysql://hetu:hetu123@db/hetudb"
+SQLALCHEMY_DATABASE_URL = "mysql+aiomysql://hetu:hetu123@db/hetudb"
 
-SQLALCHEMY_DATABASE_URL = "mysql+aiomysql://hetu:hetu123@127.0.0.1:3306/hetudb"
+#SQLALCHEMY_DATABASE_URL = "mysql+aiomysql://hetu:hetu123@127.0.0.1:3306/hetudb"
 
-#REDIS_URL = "redis://redis:6379/0"
-REDIS_URL = "redis://127.0.0.1:6379/0"
+REDIS_URL = "redis://redis:6379/0"
+#REDIS_URL = "redis://127.0.0.1:6379/0"
 
 # SQLAlchemy setup
 engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True)
